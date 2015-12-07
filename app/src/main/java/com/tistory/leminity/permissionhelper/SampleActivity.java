45ab5bf621f9488253f8add3565ecf9dc29e7638
leminity <leminity@gmail.com>
2015-12-07 17:03:46 +0900

    # implements sample

--- app/src/main/java/com/tistory/leminity/permissionhelper/SampleActivity.java
+++ app/src/main/java/com/tistory/leminity/permissionhelper/SampleActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by leminity on 2015-12-07.
@@ -15,30 +16,30 @@ import android.view.View;
  */
 public class SampleActivity extends Activity {
 
+    private static final String TAG = SampleActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
         findViewById(R.id.buttonRequestPerm).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                //Implements permission request.
                 PermissionHelper
                         .requestPermission(SampleActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE, 123)
-                        //.requestPermission(SampleActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 123)
                         .setActionGranted(new Runnable() {
                             @Override
                             public void run() {
-
+                                showToast("Permission granted");
                             }
                         })
                         .setActionDenied(new Runnable() {
                             @Override
                             public void run() {
-
+                                showToast("Permission denied");
                             }
                         })
-                        //.setActionShouldRational()
+                        .setActionShouldRational(R.string.message_read_external_storage)
                         .execute();
             }
         });
@@ -48,7 +49,6 @@ public class SampleActivity extends Activity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         //must be called when callback onRequestPermissionsResult.
         PermissionHelper.callbackPermissionResult(this, requestCode, grantResults);
-
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
@@ -58,6 +58,10 @@ public class SampleActivity extends Activity {
         //If not can cause memory leak.
         PermissionHelper.activityDestroyed(this);
         super.onDestroy();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
 }
